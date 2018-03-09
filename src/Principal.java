a00b1c42f3589263c471a464d307ae3f8ab00215
Renato <renato@localhost.localdomain>
2018-03-09 09:56:54 -0300

    Array of Integer - Sort Method

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,16 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Principal {
 
 	public static void main(String[] args) {
+
+		ArrayList<Integer> data = new ArrayList<Integer>(); // {1,1,1,3,3,3,6,8,8};
 		
-		ArrayList<Integer> data = new ArrayList<Integer>(); //{1,1,1,3,3,3,6,8};
-		
-		ArrayList<Integer> valEsperados = new ArrayList<Integer>(); //{2,4,5,7,9}
-		
-		int valEsp  = 0;
-		
+		int maior = 0;										
 		data.add(1);
 		data.add(1);
 		data.add(1);
@@ -20,44 +19,70 @@ public class Principal {
 		data.add(3);
 		data.add(6);
 		data.add(8);
+		data.add(8);
+		
+
+		ArrayList<Integer> listaAd = new ArrayList<Integer>(); 
+		
+		maior = data.get(0);
+		
+		int valorC = 0;
 		
 		for (int i = 0; i < data.size(); i++) {
+			int dif = data.get(i) - valorC ;
 			
-			valEsp = data.get(i) + 1;
-			valEsperados.add(valEsp);
+			if( dif > 1 ){
+				
+				for (int j = 1; j < dif  ; j++) {
+					valorC += 1;
+					listaAd.add(valorC);
+				}
+				
+				valorC+=1;
+			}else{
+				valorC = data.get(i);
+			}
+
+			if(data.get(i) > maior){
+				maior = data.get(i);
 				
 			}
 
-		int[ ] unicos = new int[ valEsperados.size() ];
-        int qtd = 0;
-        for( int i = 0 ; i < valEsperados.size() ; i++ ) {
-            boolean existe = false;
-            for( int j = 0 ; j < qtd ; j++ ) {
-                if( unicos[ j ] == valEsperados.get(i) ) {
-                    existe = true;
-                    break;
-                }
-            }
-            if( !existe ) {
-                unicos[ qtd++ ] = valEsperados.get(i);
-            }
-        }
+		}
 
-        // ajuste do tamanho do vetor resultante
-        unicos = Arrays.copyOf( unicos , qtd );
+		data.addAll(listaAd);
+		
+				
+		for(int i=0; i<data.size(); i++){
+			System.out.print(data.get(i));
+			
+			int codStatus = data.get(i);
+			
+			System.setProperty("codStatus",Integer.toString(codStatus));
+			
+			Collections.sort(data, new Comparator<Integer>() {
+				public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+	    });
+			
+			switch (codStatus) {
+			case 1:
+				
+				break;
 
-        
-       // int[] completo = Arrays.sort();
-        
-        // imprime resultado
-        for( int i = 0 ; i < unicos.length ; i++ ) {
-        	data.add(unicos[i]);
-            
-        }
+			default:
+				break;
+			}
+		}
+
+		
+		
+		
+		
 		
 		
 	}
-	
 	
 
 }
